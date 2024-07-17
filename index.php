70a1edb467c1db9ab4d37bd994f53a2b9d20a289
glaucoSapucaia <glaucopco@gmail.com>
2024-07-17 13:30:45 -0300

    PSR7 request e response | Testes com PUT e DELETE -> Postman

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
     $app = new \Slim\App;
 
     // routes
+
+    $app->get('/', function(Request $request, Response $response) {
+        $response->getBody()->write('Index on!');
+    });
+
     // Tipando request e response
     $app->get('/postagens', function(Request $request, Response $response) {
         // PADRÂO PSR-7
@@ -34,7 +39,30 @@
         $nome = $post['nome'];
         $email = $post['email'];
 
-        return $response->getBody()->write($nome . ' | ' . $email);
+        // Faça sua lógica para insert into DB
+
+        return $response->getBody()->write('Dados salvos no DB via post request!');
+    });
+
+    $app->put('/usuarios/atualiza', function(Request $request, Response $response) {
+        // Recuoerando dados $_POST (Postman)
+        // getParseBody() -> recupera dados de forms (post)
+        $post = $request->getParsedBody();
+        $id = $post['id'];
+        $nome = $post['nome'];
+        $email = $post['email'];
+
+        // Faça sua lógica para update DB
+
+        return $response->getBody()->write('Dados ATUALIZADOS no DB via put request!');
+    });
+
+    $app->delete('/usuarios/remove/{id}', function(Request $request, Response $response) {
+        $id = $request->getAttribute('id');
+
+        // Faça sua lógica para delete no DB
+
+        return $response->getBody()->write('Usuário removido | ID: ' . $id);
     });
 
     // init app
